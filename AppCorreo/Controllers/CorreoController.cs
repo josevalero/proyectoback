4c76fa1e8ba375a3d692303679224fcc79f13a4d
josevalero <joseavg95@hotmail.com>
2019-01-30 15:33:30 -0600

    back

---
+++ AppCorreo/Controllers/CorreoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppCorreo.Controllers
+{
+    public class CorreoController : Controller
+    {
+        // GET: Correo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EnviarCorreo()
+        {
+            return View();
+        }
+    }
+}
